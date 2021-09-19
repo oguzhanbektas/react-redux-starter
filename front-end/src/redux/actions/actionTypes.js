3d7911773b75d013af59115334dfc8243467baa5
Oğuzhan BEKTAŞ <ozibektas@gmail.com>
2021-09-20 00:13:11 +0300

    actionTypes and categoryActions

---
+++ front-end/src/redux/actions/actionTypes.js
@@ -0,0 +1,7 @@
+export const CHANGE_CATEGORY = "CHANGE_CATEGORY";
+export const GET_CATEGORY_SUCCESS = "GET_CATEGORY_SUCCESS";//apiye bağlanarak yapılan asenkron işlemlerinde Redux-Thunk kullanıyoruz ve böyle bir isimlendirme kullanıyoruz.
+
+
+
+
+
